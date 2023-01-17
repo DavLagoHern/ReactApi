bce5274a765ae926151b1c72e84a8c1137ccea55
davlago <davlago@ucm.es>
2023-01-17 10:46:15 +0100

    prueba

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>Hola mundo</div>
+  )
+}
+
+export default App
